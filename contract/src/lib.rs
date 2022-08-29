b13345945025784e29c2643334db606fc5bffa83
ggichuru <ggichuru.dev@gmail.com>
2022-08-29 12:33:12 +0300

    inintialize SC with near_metadata

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -5,7 +5,10 @@ use near_contract_standards::fungible_token::FungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LazyOption;
 use near_sdk::json_types::U128;
-use near_sdk::{env, log, near_bindgen, AccountId, Balance, PanicOnDefault, PromiseOrValue};
+use near_sdk::serde::__private::de::ContentRefDeserializer;
+use near_sdk::{
+    env, log, metadata, near_bindgen, AccountId, Balance, PanicOnDefault, PromiseOrValue,
+};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -19,6 +22,7 @@ const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://
 #[near_bindgen]
 impl Contract {
     ///
+    ///                     ***CUSTOM METADATA***
     /// Initializes the contract with the given total supply owned by the given `owner_id` with default metadata
     ///
     /// <this is for example purposes only>
@@ -40,6 +44,46 @@ impl Contract {
     }
 
     ///
-    /// initialize the contract with the given total supply owned by the given `owner_id
-    /// 
+    /// initialize the contract with the given total supply owned by the given `owner_id' wihth the given ft metadata
+    ///
+    #[init]
+    pub fn new(owner_id: AccountId, total_supply: U128, metadata: FungibleTokenMetadata) -> Self {
+        assert!(env::state_exists(), "Already initialized");
+        metadata.assert_valid();
+
+        let mut this = Self {
+            token: FungibleToken::new(b"a".to_vec()),
+            metadata: LazyOption::new(b"m".to_vec(), Some(&metadata)),
+        };
+
+        this.token.internal_register_account(&owner_id);
+        this.token.internal_deposit(&owner_id, total_supply.into());
+
+        near_contract_standards::fungible_token::events::FtMint {
+            owner_id: &owner_id,
+            amount: &total_supply,
+            memo: Some("Initial tokens supply is minted"),
+        }
+        .emit();
+
+        this
+    }
+
+    fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
+        log!("Closed @{} with {}.", account_id, balance);
+    }
+
+    fn on_tokens_burned(&mut self, account_id: AccountId, balance: Balance) {
+        log!("Account @{} burned {}", account_id, balance);
+    }
+}
+
+near_contract_standards::impl_fungible_token_core!(Contract, token, on_tokens_burned);
+near_contract_standards::impl_fungible_token_storage!(Contract, token, on_account_closed);
+
+#[near_bindgen]
+impl FungibleTokenMetadataProvider for Contract {
+    fn ft_metadata(&self) -> FungibleTokenMetadata {
+        self.metadata.get().unwrap()
+    }
 }
